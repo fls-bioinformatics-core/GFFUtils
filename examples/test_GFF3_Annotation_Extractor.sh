c9ac3f1c98409a8c5d0e028e3e12c6b8e684b9f8
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-07-29 14:55:35 +0100

    Update test scripts to enable them to run from arbitrary locations.

--- examples/test_GFF3_Annotation_Extractor.sh
+++ examples/test_GFF3_Annotation_Extractor.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 #
 # Run test of GFF3_Annotation_Extractor.py
+DATA_DIR=$(dirname $0)/data
 if [ ! -d output ] ; then
   mkdir output
 fi
-GFF3_Annotation_Extractor.py -o output/test_dicty.out --htseq-count data/dicty.gff \
- data/dicty_htseq_counts.dimA.txt data/dicty_htseq_counts.AXA4.txt
+GFF3_Annotation_Extractor.py \
+ -o output/test_dicty.out --htseq-count \
+ ${DATA_DIR}/dicty.gff \
+ ${DATA_DIR}/dicty_htseq_counts.dimA.txt \
+ ${DATA_DIR}/dicty_htseq_counts.AXA4.txt
 ##
 #
--- examples/test_GFF3_Annotation_Extractor_with_GTF.sh
+++ examples/test_GFF3_Annotation_Extractor_with_GTF.sh
@@ -2,9 +2,13 @@
 #
 # Run test of GFF3_Annotation_Extractor.py with GTF input
 # Run test of GFF3_Annotation_Extractor.py
+DATA_DIR=$(dirname $0)/data
 if [ ! -d output ] ; then
   mkdir output
 fi
-GFF3_Annotation_Extractor.py -o output/test_mm10.txt --htseq-count data/mm10.gtf data/mm10_htseq_counts.txt
+GFF3_Annotation_Extractor.py \
+ -o output/test_mm10.txt --htseq-count \
+ ${DATA_DIR}/mm10.gtf \
+ ${DATA_DIR}/mm10_htseq_counts.txt
 ##
 #
