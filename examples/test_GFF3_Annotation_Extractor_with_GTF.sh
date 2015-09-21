5649356d4b444ab2ecdd57d8d4b9d3dab06afa40
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-09-21 12:49:18 +0100

    Update the example scripts to reference the utilities without trailing '.py'.

--- examples/test_GFF3_Annotation_Extractor_with_GTF.sh
+++ examples/test_GFF3_Annotation_Extractor_with_GTF.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# Run test of GFF3_Annotation_Extractor.py with GTF input
-# Run test of GFF3_Annotation_Extractor.py
+# Run test of GFF3_Annotation_Extractor with GTF input
+# Run test of GFF3_Annotation_Extractor
 DATA_DIR=$(dirname $0)/data
 if [ ! -d output ] ; then
   mkdir output
 fi
-GFF3_Annotation_Extractor.py \
+GFF3_Annotation_Extractor \
  -o output/test_mm10.txt --htseq-count \
  ${DATA_DIR}/mm10.gtf \
  ${DATA_DIR}/mm10_htseq_counts.txt
